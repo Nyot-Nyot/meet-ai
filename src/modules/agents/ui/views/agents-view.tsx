525c564aa474e4f41a1b02b24452449284981f4e
Nyot-Nyot <dzakisultan012@gmail.com>
2025-08-08 21:03:01 +0700

    12 - Agents data table - add data table component - add empty state component

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,12 +4,25 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/column";
+import { EmptyState } from "@/components/empty-state";
 
 export const AgentView = () => {
 	const trpc = useTRPC();
 	const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-	return <div>{JSON.stringify(data, null, 2)}</div>;
+	return (
+		<div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+			<DataTable data={data} columns={columns} />
+			{data.length === 0 && (
+				<EmptyState
+					title="No Agents Found"
+					description="Create an agents to join your meetings. Each agents will follow your instructions and can interact with participants during the call."
+				/>
+			)}
+		</div>
+	);
 };
 
 export const AgentViewLoading = () => {
